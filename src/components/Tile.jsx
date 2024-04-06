f6b728599e424ae03210e2da6c86cae36f9bf989
harinarayanan-kp <harikpaug18@gmail.com>
2024-04-07 03:45:12 +0530

    update2

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import "../style.css";
+import arrow from "./arrow.svg";
 
 const Tile = () => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [delivered, setDelivered] = useState(false);
 
   const handleViewMore = () => {
     setIsExpanded(!isExpanded);
@@ -32,24 +34,34 @@ const Tile = () => {
 
   const itemList = list1.map((item) => (
     <div key={item.id} className="tile1">
-      <div className="">{item.prescription}</div>
-      <div className="">{item.dosage}</div>
-      <div className="">{item.duration}</div>
+      <div className="tile1child">{item.prescription}</div>
+      <div className="tile1child">{item.dosage}</div>
+      <div className="tile1child">{item.duration}</div>
     </div>
   ));
 
   return (
     <div className="tilemain">
       <div className="tiletop">
-        <div className="tileid">ID:{"25"}</div>
-        <div className="tileamount">50INR</div>
-        <div className="tilename">{"Name"}</div>
-        <div className="tiledoctor">{"I.m Doctor"}</div>
-        <div className="viewmore" onClick={handleViewMore}>
-          View More
+        <div className="">
+          <div className="tileid">ID:{"25"}</div>
+          {delivered ? (
+            <div className="delivered">delivered</div>
+          ) : (
+            <div className="pending">pending</div>
+          )}
         </div>
+        <div className="tilename">{"Name"}</div>
+        <div className="tiledoctor">{"Dr. Octopus"}</div>
+        <div className="tileamount">{"50.00"}₹</div>
       </div>
+
       {isExpanded && itemList}
+      <img
+        onClick={handleViewMore}
+        src={arrow}
+        className={isExpanded ? "icon-30 rotate180" : "icon-30"}
+      />
     </div>
   );
 };
