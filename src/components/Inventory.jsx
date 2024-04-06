393574ca5cade4ce32aa48474aa39a8df6b0fa4e
Gayathri Thejus <gayathrithejus@gmail.com>
2024-04-07 04:03:25 +0530

    changes made

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -3,7 +3,7 @@ import Tile from './Tile'
 const Inventory = () => {
   return (
     <div className='text-black'>
-        <div className=' max-w-[1300px] flex flex-col bg-gradient-to-r from-green-100 to-slate-50 items-center h-[1200px] mx-auto'>
+        <div className=' max-w-[1300px] flex flex-col bg-gradient-to-r from-green-100 to-slate-50 items-center h-full mx-auto'>
           <div className=''></div>
         <div className='mt-[96px] bg-black max-w-[1100px] w-full h-full rounded-3xl  bg-gray-500 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 '>
           <h1 className='text-xl font-light text-gray-600 text-center mt-12'>Dashboard</h1>
