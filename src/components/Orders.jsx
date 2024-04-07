69a0c2d874233cabab4433847c30231feaa02ba5
harinarayanan-kp <harikpaug18@gmail.com>
2024-04-07 14:47:17 +0530

    order page changed

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar2";
 import OrderTile from "./OrderTile";
+import { CiSearch } from "react-icons/ci";
 import Dropdown from "./Dropdown";
 
 const Orders = () => {
+  const [query, setQuery] = useState("");
   return (
     <div className="orderhome">
       <Navbar />
       <div className="topbar">
         <b>Inventory</b>
-        <input placeholder="search" className="searchbox"/>
-        <div className="dropdown">dropdown</div>
+        <div className="searchbox">
+          <CiSearch size={30}/>
+          <input
+            style={{
+                paddingLeft: "15px",
+              height: "30px",
+              borderRadius: "15px",
+              border: "1px solid grey",
+              fontSize: "14px",
+              width: "100%",
+            }}
+            type="text"
+            placeholder="Search by product name"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+        {/* <div className="dropdown">dropdown</div> */}
         {/* <Dropdown/> */}
       </div>
-      <OrderTile/>
-      <OrderTile/>
-      <OrderTile/>
-      <OrderTile/>
-      <OrderTile/>
+      <OrderTile />
+      <OrderTile />
+      <OrderTile />
+      <OrderTile />
+      <OrderTile />
     </div>
   );
 };
